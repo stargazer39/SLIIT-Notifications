cd6133dc50e4c3000822c473836eebe641f77e8c
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-07-15 16:50:48 +0530

    TelegramClient changes

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,5 +1,7 @@
-import { Telegraf } from 'telegraf';
-import { MongoConnect } from './mongo';
+import { Context, Telegraf } from 'telegraf';
+import { Cursor, Db, MongoClient } from 'mongodb';
+import { Update } from 'telegraf/typings/core/types/typegram';
+import { sleep } from './common';
 
 const MAX_LENGTH = 4000;
 
@@ -7,46 +9,68 @@ export class TelegramClient {
     token : string;
     bot : Telegraf;
     chatIDs : number[] = [];
-    mongoClient : MongoConnect;
+    db : Db;
 
-    constructor(token : string, mongoClient : MongoConnect){
+    constructor(token : string){
         this.token = token
-        this.mongoClient = mongoClient;
         this.bot = new Telegraf(token);
     }
 
-    async launch() {
-        let conf  = await this.mongoClient.findOne("config", { type:"config" }) as any;
-        this.chatIDs = conf.chatIDs;
+    setdb(db : Db){
+        this.db = db;
+    }
 
-        console.log("Configiured telegram with ", conf);
-        this.bot.command('config', async (ctx) => {
-            let res  = await this.mongoClient.findOne("config", { type:"config", "chatIDs":{ $in:[ctx.chat.id] } });
-            console.log(res);
-            if(!res){
-                await this.mongoClient.updateOne("config", { type:"config" }, { $push:{ "chatIDs":ctx.chat.id } })
-                ctx.reply("This group have been added to notification list.");
-                if(!this.chatIDs.includes(ctx.chat.id)) {
-                    this.chatIDs.push(ctx.chat.id);
-                }
-            }else{
-                ctx.reply("This group is already in the database.");
-            }
-            
-        })
-        this.bot.launch()
+    setConfig(config : any) {
+        this.chatIDs = config.chatIDs;
+    }
+
+    async launch() {
+        // Add event listeners 
+        this.bot.command('config', this._onConfig.bind(this));
+        this.bot.launch();
 
         // Enable graceful stop
         // process.once('SIGINT', () => this.bot.stop('SIGINT'))
         // process.once('SIGTERM', () => this.bot.stop('SIGTERM'))
     }
-    send(message : string){
-        for(const c of this.chatIDs){
-            let count = 0;
-            while(count < message.length){
-                this.bot.telegram.sendMessage(c,message.substring(count,count + MAX_LENGTH));
-                count += MAX_LENGTH;
+
+    async _onConfig(ctx : Context<Update>) {
+        console.log("onConfig");
+        // Check if this chat id exsists
+        try {
+            const thisId = ctx.chat.id;
+            if(this.chatIDs.includes(thisId)){
+                ctx.reply("This group is already in the database.");
+                return;
             }
+
+            let config = await this.db.collection("config").findOne({ type:"config" });
+            if(!config.chatIDs.includes(thisId)) {
+                await this.db.collection("config").updateOne({ type:"config" }, { $push:{ chatIDs:thisId } });
+                this.chatIDs.push(thisId);
+                ctx.reply("This group have been added to notification list.");
+            }else{
+                ctx.reply("Chat in db but not in server");
+            }
+        }catch(e){
+            ctx.reply("Configureation faild. Bot had a stroke.")
+            console.dir(e);
+        }        
+    }
+
+    async send(message : string){
+        for(const c of this.chatIDs){
+            try {
+                let count = 0;
+                while(count < message.length){
+                    await this.bot.telegram.sendMessage(c,message.substring(count,count + MAX_LENGTH));
+                    count += MAX_LENGTH;
+                }
+            }catch(e){
+                console.log(`Sending to chatId ${c} faild with :`);
+                console.dir(e);
+            }
+            await sleep(1000);
         }
     }
 }
